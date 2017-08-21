bf2dd2bd5f46ed230fa5440214ba7e85eec2a960
Freddy Cheungh <fgcc1111@gmail.com>
2017-08-21 02:42:05 -0400

    Core Elements
    Health, events, text UI Hud. Missing two chapters.

--- Survival Shooter/Assets/Scripts/Player/PlayerHealth.cs
+++ Survival Shooter/Assets/Scripts/Player/PlayerHealth.cs
@@ -3,72 +3,74 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
+public class PlayerHealth : MonoBehaviour {
 
-public class PlayerHealth : MonoBehaviour
-{
     public int startingHealth = 100;
     public int currentHealth;
-    public Slider healthSlider;
+    //public Slider healthSlider;
     public Image damageImage;
     public AudioClip deathClip;
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
+    public Text healthText;
 
     Animator anim;
     AudioSource playerAudio;
     PlayerMovement playerMovement;
-    //PlayerShooting playerShooting;
+    PlayerShooting playerShooting;
     bool isDead;
     bool damaged;
 
+    void Awake () {
 
-    void Awake ()
-    {
-        anim = GetComponent <Animator> ();
-        playerAudio = GetComponent <AudioSource> ();
-        playerMovement = GetComponent <PlayerMovement> ();
-        //playerShooting = GetComponentInChildren <PlayerShooting> ();
+        anim = GetComponent <Animator>();
+        playerAudio = GetComponent <AudioSource>();
+        playerMovement = GetComponent <PlayerMovement>();
+        playerShooting = GetComponentInChildren <PlayerShooting>();
         currentHealth = startingHealth;
+
     }
 
+    void Update () {
 
-    void Update ()
-    {
-        if(damaged)
-        {
-            damageImage.color = flashColour;
-        }
-        else
-        {
-            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
+        damageImage.color = damaged ? flashColour : Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+
+        //if (damaged)
+        //{
+        //    damageImage.color = flashColour;
+        //}
+        //else
+        //{
+        //    damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        //}
+
         damaged = false;
+
     }
 
+    public void TakeDamage (int amount) {
 
-    public void TakeDamage (int amount)
-    {
         damaged = true;
-
         currentHealth -= amount;
+        //healthSlider.value = currentHealth;
+        healthText.text = currentHealth.ToString();
+        healthText.color = (currentHealth <= 70 && currentHealth > 30) ? new Color(1f, 0.6f, 0f) : (currentHealth <= 30 ? Color.red : new Color(0f, 1f, 0f));
+        playerAudio.Play();
 
-        healthSlider.value = currentHealth;
+        if(currentHealth <= 0 && !isDead) {
 
-        playerAudio.Play ();
+            Death();
 
-        if(currentHealth <= 0 && !isDead)
-        {
-            Death ();
         }
+
     }
 
+    void Death () {
 
-    void Death ()
-    {
         isDead = true;
 
-        //playerShooting.DisableEffects ();
+        playerShooting.DisableEffects ();
 
         anim.SetTrigger ("Die");
 
@@ -76,12 +78,14 @@ public class PlayerHealth : MonoBehaviour
         playerAudio.Play ();
 
         playerMovement.enabled = false;
-        //playerShooting.enabled = false;
+        playerShooting.enabled = false;
+
     }
 
+    public void RestartLevel () {
 
-    public void RestartLevel ()
-    {
-        SceneManager.LoadScene (0);
+        SceneManager.LoadScene(0);
+
     }
+
 }
